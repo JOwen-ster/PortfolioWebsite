f1164e32de191dc21391470e062e0319d5d2e729
Owen Sterling <jowen.sterling@csu.fullerton.edu>
2025-02-07 15:34:43 -0800

    Update

--- portfolio/src/routes/+page.svelte
+++ portfolio/src/routes/+page.svelte
@@ -1,8 +1,54 @@
 <script>
     import './global.css';
+    import Navbar from '$lib/Components/Navbar.svelte'
     import Hero from '$lib/Components/Hero.svelte';
 </script>
 
-<h1>
-    <Hero/>
-</h1>
+<Navbar/>
+
+<Hero content={'Owen Sterling'}/>
+
+<p id="home">Home</p>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<p id="skills">Skills</p>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<p id="projects">Projects</p>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<p id="contact">Contact Me</p>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
